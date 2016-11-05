13882b33ab21b0dcfa8c52f92c5d10ed91ac97b9
3ebs <yousefahmedkamal94@gmail.com>
2016-11-05 18:17:41 +0200

    commented self.stream.release() because it caused error - add sample main code - worked with camera 0

--- streaming_HT.py
+++ streaming_HT.py
@@ -10,19 +10,40 @@ arg.add_argument('-t', '--threading', type=str, default="no", help="Enable or di
 args = vars(arg.parse_args())
 '''**********************************'''
 '''**********Streaming Thread*********'''
-class VideoStream(object, threading.Thread):
+class VideoStream(threading.Thread):
     def __init__(self, src=0):
+        threading.Thread.__init__(self)
         self.stream = cv2.VideoCapture(src)
-        (self.grabbed, self.frame) = self.stream.read()
-        self.stopStream = False
+        self.stop = False
+        self.frame = None
+    def stopStream(self):
+        #self.stream.release()
+        self.stop = True
     def run(self):
         while True:
-            if(self.stopStream):
+            if(self.stop):
                 return
             (self.grabbed, self.frame) = self.stream.read()
-    def stopStream(self):
-        self.stream.release()
-        self.stopStream = True
+            if not self.grabbed:
+                print "[INFO] End of streaming.."
+                self.stopStream()
+    def readFrame(self):
+        return self.frame
+    def streamFlag(self):
+        return self.stop
 '''**************************************'''
+'''***************main code**************'''
+print "[INFO] Starting streaming.."
+video = VideoStream(src=0) #decide video source
+video.start() #start thread of streaming
+while True:
+    frame = video.readFrame()
+    if frame is None:
+        continue
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q') or video.streamFlag():
+        break
 
+print "[INFO] End of stream.."
 cv2.destroyAllWindows() #End of code
+video.stopStream()
