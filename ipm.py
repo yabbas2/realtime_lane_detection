80ef32a284d8d186ee67d47f4362007915b9b652
amal <amal.elsayed43@gmail.com>
2016-11-21 11:35:31 +0000

    Update ipm.py

--- ipm.py
+++ ipm.py
@@ -1,6 +1,5 @@
-# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]"
-# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]"
-# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]"
+# python transform_example.py --image path of image
+
 
 # import the necessary packages
 
