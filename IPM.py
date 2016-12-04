72507e94f212282ad4066bf697066ff3c9ac10c2
3ebs <yousefahmedkamal94@gmail.com>
2016-12-04 22:02:30 +0200

    remove main code - move pts array to main.py

--- IPM.py
+++ IPM.py
@@ -1,11 +1,4 @@
-# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]"
-# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]"
-# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]"
-
-# import the necessary packages
-
 import numpy as np
-import argparse
 import cv2
 
 
@@ -97,7 +90,7 @@ def four_point_transform(image, pts):
     #row, col = image.shape[:2]
 
     #image[(row / 2 + row / 7):row, 0:col] = original2
-    cv2.imshow("Original", image)
+    #cv2.imshow("Original", image)
     # cropped = image[0:w,  0:h]
 
     '''
@@ -117,40 +110,3 @@ def four_point_transform(image, pts):
 
     # return the warped image
     return warped
-
-
-# construct the argument parse and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", help="path to the image file")
-# ap.add_argument("-c", "--coords",
-#	help = "comma seperated list of source points")
-#args = vars(ap.parse_args())
-
-# load the image and grab the source coordinates (i.e. the list of
-# of (x, y) points)
-# NOTE: using the 'eval' function is bad form, but for this example
-# let's just roll with it -- in future posts I'll show you how to
-# automatically determine the coordinates without pre-supplying them
-image = cv2.imread("lane3.png")
-# pts = np.array(eval(args["coords"]), dtype = "float32")
-
-h, w, channels = image.shape
-'''
-pts = np.array([[0, h / 2],
-                [w, h / 2],
-                [w, h],
-                [0, h]], dtype="float32")
-'''
-pts = np.array([[263, 142],
-                [394, 142],
-                [485, 209],
-                [107, 208]], dtype="float32")
-# apply the four point tranform to obtain a "birds eye view" of
-# the image
-warped = four_point_transform(image, pts)
-# show the original and warped images
-
-
-cv2.imshow("Warped", warped)
-
-cv2.waitKey(0)
