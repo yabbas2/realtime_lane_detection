297e74431d4011120e14e23346dd9afa1dc4f1ae
3ebs <yousefahmedkamal94@gmail.com>
2016-12-04 22:21:43 +0200

    add noise removal

--- gabor.py
+++ gabor.py
@@ -10,4 +10,11 @@ def build_gabor_filter():
 
 def process(img, filter):
     filtered_img = cv2.filter2D(img, cv2.CV_8UC3, filter)
+
+    filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
+    filtered_img = cv2.GaussianBlur(filtered_img, (19, 19), 0)
+    filtered_img = cv2.adaptiveThreshold(filtered_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    filtered_img = cv2.morphologyEx(filtered_img, cv2.MORPH_OPEN, kernel, iterations=1)
+
     return filtered_img
