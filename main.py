17fd24020cdde3ca452d5875defdcfcbfacc481f
Omnia <Omnia>
2016-12-06 01:31:16 +0200

    modify pts for four videos

--- main.py
+++ main.py
@@ -18,20 +18,14 @@ print "[INFO] Start of streaming.."
 '''*****************************Initialization*******************'''
 if args['video'].endswith("sample7.mp4"):
     pts = np.array([[263, 142], [394, 142], [485, 209], [107, 208]], dtype="float32")
-'''elif args['video'].endswith("sample5_cut_1.mp4"):
-    pts =
-elif args['video'].endswith("sample5_cut_2.mp4"):
-    pts =
-elif args['video'].endswith("sample5_cut_3.mp4"):
-    pts =
-'''
+else:
+    pts = np.array([[269, 206], [384, 206], [596, 297], [1, 297]], dtype="float32")
 houghOutput = np.zeros((200, 200, 3), dtype=np.uint8)
 if args['video'] is not None:
     video_in = VideoStreamIn(src=args['video'])
 else:
     video_in = VideoStreamIn(src=0)
 video_out = VideoStreamOut()
-
 video_in.start()
 video_out.start()
 '''**************************************************************'''
@@ -47,7 +41,7 @@ while True:
     if frame is None:
         continue
     '''*******************************IPM************************************'''
-    ipmInput = frame.copy()
+    ipmInput = frame
     ipmOutput = IPM.four_point_transform(ipmInput, pts)
     '''****************************Gabor Filter******************************'''
     gaborInput = ipmOutput
@@ -64,6 +58,7 @@ while True:
     except:
         pass
     '''*************************Final Step***********************************'''
+    # @amal @amany: call the required function here - your input is houghOutput
     finalOutput = IPM.inverse(ipmInput,houghOutput,pts)
     '''**************************Displaying Videos***************************'''
     if args['show'] == 'gabor':
@@ -74,7 +69,5 @@ while True:
         video_out.showFrame(houghOutput)
     elif args['show'] == 'all':
         video_out.showFrame(finalOutput)
-    elif args['show'] == 'original':
-        video_out.showFrame(frame)
 
 #End of program
