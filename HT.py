b612ff5f30fa13a706c9a35e471752e27e777fcc
3ebs <yousefahmedkamal94@gmail.com>
2016-12-18 12:19:16 +0200

    use PHT without futher algorithm

--- HT.py
+++ HT.py
@@ -4,28 +4,15 @@ import numpy as np
 
 def HoughTransform(input, houghOutput):
     h, w = input.shape[:2]
-    # using PHT
-
     minLineLength = 60  # 50
     maxLineGap = 1  # 5
     maxVotes = 90  # 50
     lines = cv2.HoughLinesP(input, 1, np.pi / 180, maxVotes, minLineLength, maxLineGap)
-    array_of_lines = [[0 for i in range(w)] for j in range(h)]
-    percentage = list()
-    x = 0
     for line in lines:
         for x1, y1, x2, y2 in line:
-            if not (abs(x1 - x2) < 1 and x1 > 15 and x1 < w-30):  # 0 and abs(x1 - x2) < ):  #catch vertical lines
+            if not (x1 > 15 and x1 < w - 30):
                 continue
-            # print x1, x2, y1, y2  # debugging
-            array_of_lines[x][x1] = x1
-            x = (x + 1) % w
-    percentage = calculateMax(array_of_lines, h, w)
-    for i in range(0, len(percentage), 1):
-        sumP = percentage[i]  # sum(percentage[i:i+17])
-        avg_i = i  # ((i * 16) + 120) / 16
-        if sumP > 0.4:
-            cv2.line(houghOutput, (avg_i, 0), (avg_i, h), (0, 0, 255), 4)
+            cv2.line(houghOutput, (x1, y1), (x2, y2), (0, 255, 0), 1)
     return houghOutput
 
 
