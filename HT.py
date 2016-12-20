fff5d8fe179b6424f34dc0f32b5d536b2496aea1
3ebs <yousefahmedkamal94@gmail.com>
2016-12-21 00:57:06 +0200

    modify range of line detection - modify findLines method

--- HT.py
+++ HT.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, math
 import numpy as np
 
 
@@ -11,17 +11,17 @@ def HoughTransform(input, height):
     lines = cv2.HoughLinesP(input, 1, np.pi / 180, maxVotes, minLineLength, maxLineGap)
     for line in lines:
         for x1, y1, x2, y2 in line:
-            if not (x1 > 15 and x1 < w - 30):
+            if not (x1 > 0 and x1 < w):
                 continue
             phaseOneFilteredLines.append([x1, y1, x2, y2])
     #Stage one
-    phaseTwoFilteredLines = averaging(15, w - 30, 10, phaseOneFilteredLines, 0)
+    phaseTwoFilteredLines = averaging(0, w, 10, phaseOneFilteredLines, 0)
     #Stage two
-    phaseThreeFilteredLines = averaging(15, w - 30, 20, phaseTwoFilteredLines, 0)
+    phaseThreeFilteredLines = averaging(0, w, 20, phaseTwoFilteredLines, 0)
     #Stage three
-    phaseThreeFilteredLines = averaging(15, w - 30, 40, phaseThreeFilteredLines, 0)
+    phaseThreeFilteredLines = averaging(0, w, 40, phaseThreeFilteredLines, 0)
     #Stage Four
-    phaseThreeFilteredLines = averaging(15, w - 30, 80, phaseThreeFilteredLines, 0)
+    phaseThreeFilteredLines = averaging(0, w, 80, phaseThreeFilteredLines, 0)
 
     phaseFourFilteredLines = findLines(phaseThreeFilteredLines, height)
     return phaseFourFilteredLines
@@ -36,13 +36,15 @@ def averaging(start, end, windowSize, _2D_array, threshold):
         sum_y1 = float()
         sum_y2 = float()
         for x in range(i, i + windowSize, 1):
-            flag, x_1, y_1, x_2, y_2 = searchForLine(x, _2D_array)
+            flag, detectedLines = searchForLine(x, _2D_array)
             if flag:
-                counter += 1
-                sum_x1 += x_1
-                sum_x2 += x_2
-                sum_y1 += y_1
-                sum_y2 += y_2
+                for line in detectedLines:
+                    [x_1, y_1, x_2, y_2] = line
+                    counter += 1
+                    sum_x1 += x_1
+                    sum_x2 += x_2
+                    sum_y1 += y_1
+                    sum_y2 += y_2
         if counter > threshold:
             avg_x1 = sum_x1 / counter
             avg_x2 = sum_x2 / counter
@@ -54,28 +56,27 @@ def averaging(start, end, windowSize, _2D_array, threshold):
 def findLines(lines, height):
     returnLines = list()
     for line in lines:
-        perfectVertical = False
         [x1, y1, x2, y2] = line
-        try:
-            slope = (y2-y1)/(x2-x1)
-        except ZeroDivisionError:
-            perfectVertical = True
-        finally:
-            if(perfectVertical):
-                x = np.float32(x1)
-                returnLines.append([x, 0, x, height])
-                #cv2.line(image, (x, 0), (x, h), (0, 255, 0), 4)
-            else:
-                new_x1 = np.float32((0-y1)/slope + x1)
-                new_x2 = np.float32((height-y1)/slope + x1)
-                returnLines.append([new_x1, 0, new_x2, height])
-                #cv2.line(image, (new_x1, 0), (new_x2, h), (0, 255, 0), 4)
+        if (x1-x2) == 0:
+            x = np.float32(x1)
+            returnLines.append([x, 0, x, height])
+        else:
+            slope = (y2 - y1) / (x2 - x1)
+            theta = math.atan(slope)
+            if theta > (95 * np.pi / 180) or theta < (85 * np.pi / 180):
+                continue
+            new_x1 = np.float32((0 - y1) / slope + x1)
+            new_x2 = np.float32((height - y1) / slope + x1)
+            returnLines.append([new_x1, 0, new_x2, height])
     return returnLines
 
 
 def searchForLine(x, filtered_lines):
+    detectedLines = list()
     for line in filtered_lines:
         [x1, y1, x2, y2] = line
         if int(x1) == x:
-            return True, x1, y1, x2, y2
-    return False, 0, 0, 0, 0
+            detectedLines.append([x1, y1, x2, y2])
+    if(len(detectedLines) > 0):
+        return True, detectedLines
+    return False, [0, 0, 0, 0]
