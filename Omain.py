ca68ce31d077634deb53e028a81c2c2bd305da8d
3ebs <yousefahmedkamal94@gmail.com>
2016-12-20 21:20:18 +0200

    test version

--- Omain.py
+++ Omain.py
@@ -22,10 +22,10 @@ print "[INFO] Start of streaming.."
 if args['show2'] is not None:
     frames = args['show2'].split(',')
 
-if args['video'].startswith("sample7"):
+if args['video'].endswith("sample7.mp4"):
     pts = np.array([[263, 142], [394, 142], [485, 209], [107, 208]], dtype="float32")
-elif args['video'].startswith("sample8"):
-    pts = np.array([[271, 247], [370, 247], [468, 281], [168, 281]], dtype="float32")
+elif args['video'].endswith("sample8.mp4"):
+    pts = np.array([[236, 205], [406, 205], [498, 253], [86, 253]], dtype="float32")
 else:
     pts = np.array([[269, 206], [384, 206], [596, 297], [1, 297]], dtype="float32")
 
@@ -62,21 +62,20 @@ while True:
     gtnOutput = GTN.process(gtnInput)
     '''***************************Hough Transform****************************'''
     try:
-        houghInput1 = gtnOutput
-        houghInput2 = ipmOutput.copy()
-        houghOutput = HT.HoughTransform(houghInput1, houghInput2)
+        houghInput = gtnOutput
+        houghOutput = HT.HoughTransform(houghInput, frame.shape[0])
     except:
         pass
     '''**********************Line Segment Detector***************************'''
-    lsdInput = houghOutput.copy()
-    lsdOutput = LSD.LineSegmentDetector(lsdInput)
+    #lsdInput = houghOutput.copy()
+    #lsdOutput = LSD.LineSegmentDetector(lsdInput)
     '''*************************Final Step***********************************'''
     # @amal @amany: call the required function here - your input is houghOutput
     #finalOutput = IPM.inverse(ipmInput,houghOutput,pts)
-    finalOutput = IPM.inverse(ipmInput, lsdOutput, pts)
+    #finalOutput = IPM.inverse(ipmInput, lsdOutput, pts)
     '''**************************Displaying Videos***************************'''
-    display = {'gabor': gaborOutput, 'ht': houghOutput, 'lsd': lsdOutput, 'gtn': gtnOutput, 'ipm': ipmOutput,
-               'original': frame, 'all': finalOutput}
+    display = {'gabor': gaborOutput,'gtn': gtnOutput, 'ipm': ipmOutput,
+               'original': frame,}
     if args['show'] is not None:
         video_out.showFrame(display[args['show']])
     elif args['show2'] is not None:
