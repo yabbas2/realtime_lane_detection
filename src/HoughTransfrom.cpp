9d7d98e496809e20f3d3bbb0a3b85d9433c376fb
3ebs <yousefahmedkamal94@gmail.com>
2017-01-16 11:32:02 +0200

    add size variable - add size to private of omp - remove counter variable - modify logic of averaging

--- src/HoughTransfrom.cpp
+++ src/HoughTransfrom.cpp
@@ -15,41 +15,39 @@ void Processing::houghTransform()
 void Processing::takeAverage(int start, int end, unsigned int windowSize, unsigned int threshold)
 {
     list<Vec4d> detectedList;
-    unsigned int counter;
     int height = ipmFrame.size().height;
     double sum_x1, sum_x2, avg_x1, avg_x2;
     double slope;
+    unsigned int size;
     int i;
     size_t j;
     list<Vec4d>::iterator line;
     detectedLanes.clear();
-    #pragma omp parallel for default(none) private(detectedList, counter, sum_x1, sum_x2, avg_x1, avg_x2, slope,\
-    i, j, line) shared(height, threshold, windowSize, start, end)
+    #pragma omp parallel for default(none) private(detectedList, sum_x1, sum_x2, avg_x1, avg_x2, slope,\
+    i, j, line, size) shared(height, threshold, windowSize, start, end)
     for(i = start; i <= end; i += windowSize)
     {
-        counter = 0; sum_x1 = 0.0; sum_x2 = 0.0;
+        sum_x1 = 0.0; sum_x2 = 0.0;
         detectedList.clear();
         for(j = 0; j < lines.size(); j++)
         {
-            if(lines[j][0] >= i && lines[j][0] <= i+windowSize)
+            if(lines[j][0] >= i && lines[j][0] < i+windowSize)
                 detectedList.push_back(lines[j]);
         }
-        if(!detectedList.empty())
+        size = (unsigned int)detectedList.size();
+        if(size < threshold)
+            continue;
+        for(line = detectedList.begin(); line != detectedList.end(); line++)
         {
-            for(line = detectedList.begin(); line != detectedList.end(); line++)
-            {
-                counter++;
-                sum_x1 += (*line)[0]; sum_x2 += (*line)[2];
-            }
+            sum_x1 += (*line)[0]; sum_x2 += (*line)[2];
         }
-        if(counter > threshold)
+        avg_x1 = sum_x1/size; avg_x2 = sum_x2/size;
+        slope = atan2(height - 0, avg_x2 - avg_x1);
+        if (abs(slope) <= 95 * M_PI / 180 && abs(slope) >= 85 * M_PI / 180)
         {
-            avg_x1 = sum_x1/counter; avg_x2 = sum_x2/counter;
-            slope = atan2(height - 0, avg_x2 - avg_x1);
             #pragma omp critical
             {
-                if (abs(slope) <= 95 * M_PI / 180 && abs(slope) >= 85 * M_PI / 180)
-                    detectedLanes.push_back(Vec4d{avg_x1, 0, avg_x2, (double) height});
+                detectedLanes.push_back(Vec4d{avg_x1, 0, avg_x2, (double) height});
             }
         }
     }
