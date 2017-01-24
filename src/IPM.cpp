220a3c3e117c0cf59d6420303bcb109a2cdc07fa
amanymohammad <amyymustafa11@gmail.com>
2017-01-24 21:11:47 +0200

    IPM done

--- src/IPM.cpp
+++ src/IPM.cpp
@@ -24,12 +24,12 @@ void Processing:: inverse()
         float Z,px1,py1,px2,py2;
         Vec4d x = detectedLanes.front();
         detectedLanes.pop_front();
-        Z = 1 / (HomographyToOriginal.at(2,0) * x[0] + HomographyToOriginal.at(2,1) * x[1] + HomographyToOriginal.at(2,2));
-        px1 = ((HomographyToOriginal.at(0,0) * x[0] + HomographyToOriginal.at(0,1) * x[1] + HomographyToOriginal.at(0,2)) * Z);
-        py1 = ((HomographyToOriginal.at(1,0) * x[0] + HomographyToOriginal.at(1,1) * x[1] + HomographyToOriginal.at(1,2)) * Z);
-        Z = 1 / (HomographyToOriginal.at(2,0) * x[2] + HomographyToOriginal.at(2,1) * x[3] + HomographyToOriginal.at(2,2));
-        px2 = ((HomographyToOriginal.at(0,0) * x[2] + HomographyToOriginal.at(0,1) * x[3] + HomographyToOriginal.at(0,2)) * Z);
-        py2 = ((HomographyToOriginal.at(1,0) * x[2] + HomographyToOriginal.at(1,1) * x[3] + HomographyToOriginal.at(1,2)) * Z);
+        Z = 1 / (HomographyToOriginal.at<double>(2,0) * x[0] + HomographyToOriginal.at<double>(2,1) * x[1] + HomographyToOriginal.at<double>(2,2));
+        px1 = ((HomographyToOriginal.at<double>(0,0) * x[0] + HomographyToOriginal.at<double>(0,1) * x[1] + HomographyToOriginal.at<double>(0,2)) * Z);
+        py1 = ((HomographyToOriginal.at<double>(1,0) * x[0] + HomographyToOriginal.at<double>(1,1) * x[1] + HomographyToOriginal.at<double>(1,2)) * Z);
+        Z = 1 / (HomographyToOriginal.at<double>(2,0) * x[2] + HomographyToOriginal.at<double>(2,1) * x[3] + HomographyToOriginal.at<double>(2,2));
+        px2 = ((HomographyToOriginal.at<double>(0,0) * x[2] + HomographyToOriginal.at<double>(0,1) * x[3] + HomographyToOriginal.at<double>(0,2)) * Z);
+        py2 = ((HomographyToOriginal.at<double>(1,0) * x[2] + HomographyToOriginal.at<double>(1,1) * x[3] + HomographyToOriginal.at<double>(1,2)) * Z);
 
         detectedLanes.push_back(Vec4d{px1,py1,px2,py2});
 
