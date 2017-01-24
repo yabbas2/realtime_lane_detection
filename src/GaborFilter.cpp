b21ac210f393ea8722b6d28105783c1588c533ab
3ebs <yousefahmedkamal94@gmail.com>
2017-01-25 01:23:07 +0200

    working version - single vertical angle

--- src/GaborFilter.cpp
+++ src/GaborFilter.cpp
@@ -2,24 +2,15 @@
 
 using namespace cv;
 
-Mat Processing::gaborFilter[3];
+Mat Processing::gaborFilter;
 
 void Processing::buildGaborFilter(){
     int ksize = 31;
-    // kernel = getGaborKernel(Size(kernel_size,kernel_size), sig, th, lm, gm, ps);
-    int theta [3] = {359, 0, 1};
-    for (int i = 0; i < 3 ; ++i) {
-        gaborFilter[i] = getGaborKernel(Size(ksize,ksize), 4, theta[i], 10,0, 0);
-    }
-    //gaborFilter /= 1.5 * sum(gaborFilter)[0];
+    gaborFilter = getGaborKernel(Size(ksize,ksize), 4, 0, 10, 0, 0);
 }
 
 void Processing::gaborProcess(){
-    Mat maxFrame = ipmFrame.clone();
-    Mat tmp = ipmFrame.clone();
-    for (int i = 0; i < 3 ; ++i) {
-        filter2D(ipmFrame, tmp, CV_32F, gaborFilter[i]);
-        maxFrame = max(maxFrame, tmp);
-    }
-    ipmFrame = maxFrame.clone();
+    ipmFrame.convertTo(ipmFrame, CV_32F);
+    filter2D(ipmFrame, ipmFrame, CV_32F, gaborFilter);
+    ipmFrame.convertTo(ipmFrame,CV_8U, 10.0/255.0);
 }
