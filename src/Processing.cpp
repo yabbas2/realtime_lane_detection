e5aef798f369e17952292747bcfb9b8641bf97c7
3ebs <yousefahmedkamal94@gmail.com>
2017-01-25 01:32:44 +0200

    keep laneDetection method only - add steps of algorithm with calculated time of each step

--- src/Processing.cpp
+++ src/Processing.cpp
@@ -1,18 +1,23 @@
 #include "Processing.hpp"
 
-Mat Processing::normalFrame;
-Mat Processing::ipmFrame;
 
-void Processing::setFrame(Mat f)
-{
-    normalFrame = f;
-    ipmFrame = f;
-}
 void Processing::laneDetection()
 {
-    
-}
-list<Vec4d> * Processing::getLanesPositions()
-{
-    return &detectedLanes;
+    if(normalFrame.empty())
+        return;
+    double s1 = getTickCount();
+    fourPointTransform();
+    double d1 = (double(getTickCount()) - s1) * 1000 / getTickFrequency();
+    double s2 = getTickCount();
+    preLSD();
+    LSD();
+    double d2 = (double(getTickCount()) - s2) * 1000 / getTickFrequency();
+    double s3 = getTickCount();
+    //preHT();
+    //houghTransform();
+    double d3 = (double(getTickCount()) - s3) * 1000 / getTickFrequency();
+    double s4 = getTickCount();
+    inverse();
+    double d4 = (double(getTickCount()) - s4) * 1000 / getTickFrequency();
+    cout << d1 << "\t" << d2 << "\t" << d3 << "\t" << d4 << endl;
 }
