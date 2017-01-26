62d17a4c569a7c2d33ed7b5b6b678f3be6f3164b
3ebs <yousefahmedkamal94@gmail.com>
2017-01-26 15:20:45 +0200

    comment usage of variable 'framesNumber - add usage of videoSourceNum to help determine pts - add usage of waitFlag to synchronize between processing and streaming - remove method 'getFrameCount'

--- src/Stream.cpp
+++ src/Stream.cpp
@@ -4,18 +4,22 @@ const char Processing::endStream = 'q';
 Mat Processing::normalFrame;
 VideoCapture Processing::cap;
 double Processing::fps = 0;
-double Processing::framesNumber = 0;
+//double Processing::framesNumber = 0;
+char Processing::videoSourceNum;
 
 bool Processing::setVideoSource(string source)
 {
     if(source.length() == 1)
         cap = VideoCapture(stoi(source));
     else
+    {
+        videoSourceNum = source[source.length()-5];
         cap = VideoCapture(source);
+    }
     if(!cap.isOpened())
         return false;
     fps = cap.get(CV_CAP_PROP_FPS);
-    framesNumber = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    //framesNumber = cap.get(CV_CAP_PROP_FRAME_COUNT);
     return true;
 }
 
@@ -33,21 +37,20 @@ void Processing::videoIOStream()
             break;
         if((waitKey((int)fps) & 0xFF) == (int)endStream)
             break;
-        if(detectedLanes.empty())
-            goto init;
-        for(lane = detectedLanes.begin(); lane != detectedLanes.end(); lane++)
-            line(frameToShow, Point((int)(*lane)[0], (int)(*lane)[1]),
-                        Point((int)(*lane)[2], (int)(*lane)[3]),
-                                                Scalar(0,255,0), 3, CV_AA);
-init:   imshow("Video", frameToShow);
+        if(!detectedLanes.empty() && waitFlag)
+        {
+            for (lane = detectedLanes.begin(); lane != detectedLanes.end(); lane++)
+            {
+                line(frameToShow, Point((int)(*lane)[0], (int)(*lane)[1]),
+                                     Point((int)(*lane)[2], (int)(*lane)[3]),
+                                             Scalar(0, 255, 0), 3, CV_AA);
+            }
+            waitFlag = false;
+        }
+        imshow("Video", frameToShow);
     }
     cout << "[INFO] End of streaming!" << endl;
     cap.release();
     destroyWindow("Video");
     exit(0);
-}
-
-double Processing::getFrameCount()
-{
-    return framesNumber;
 }
