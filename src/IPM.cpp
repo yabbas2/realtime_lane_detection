6527e6698bc19313a0b3014432f53767e00816ff
3ebs <yousefahmedkamal94@gmail.com>
2017-01-26 18:36:01 +0200

    in inverse method => make lines from the bottom of screen to the farthest known points

--- src/IPM.cpp
+++ src/IPM.cpp
@@ -16,9 +16,11 @@ void Processing::fourPointTransform()
 void Processing::inverse()
 {
     unsigned int size = (unsigned int)detectedLanes.size();
+    int height = normalFrame.rows;
+    double Z, px1, py1, px2, py2;
+    float slope;
     for(unsigned int i = 0; i < size; i++)
     {
-        double Z, px1, py1, px2, py2;
         Vec4d x = detectedLanes.front();
         detectedLanes.pop_front();
         Z = 1 / (HomographyToOriginal.at<double>(2,0) * x[0] + HomographyToOriginal.at<double>(2,1) * x[1] + HomographyToOriginal.at<double>(2,2));
@@ -28,7 +30,11 @@ void Processing::inverse()
         px2 =  ((HomographyToOriginal.at<double>(0,0) * x[2] + HomographyToOriginal.at<double>(0,1) * x[3] + HomographyToOriginal.at<double>(0,2)) * Z);
         py2 =  ((HomographyToOriginal.at<double>(1,0) * x[2] + HomographyToOriginal.at<double>(1,1) * x[3] + HomographyToOriginal.at<double>(1,2)) * Z);
 
-        detectedLanes.push_back(Vec4d{px1, py1, px2, py2});
+        slope = (float)(py2 - py1) / (float)(px2 - px1);
+        px2 = (height - py1) / slope + px1;
+        py2 = height;
+
+        detectedLanes.push_back(Vec4f{(float)px1, (float)py1, (float)px2, (float)py2});
         CV_Assert(detectedLanes.size() == size);
     }
 }
