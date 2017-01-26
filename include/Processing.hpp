8a05936a63da39395bffc348bb35c8248a18c46b
3ebs <yousefahmedkamal94@gmail.com>
2017-01-26 15:23:38 +0200

    remove all methods and variables used in HT and Gabor - add variable waitFlag for sync. - comment variable 'framesNumber' - remove method 'getFrameCount'

--- include/Processing.hpp
+++ include/Processing.hpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <list>
 #include <math.h>
-#include <opencv2/core/matx.hpp>
 #include <iostream>
 
 using namespace cv;
@@ -18,30 +17,24 @@ class Processing
         static Mat ipmFrame;
         static Mat pts;
         static Mat dst;
-        static Mat gaborFilter;
+        static bool waitFlag;
         static Mat HomographyToOriginal;
         static vector<Vec4d> lines;
         static list<Vec4d> detectedLanes;
         static VideoCapture cap;
+        static char videoSourceNum;
         static double fps;
-        static double framesNumber;
+        //static double framesNumber;
         static const char endStream;
-        static void buildGaborFilter();
-        static void gaborProcess();
-        static void preHT();
-        static void preLSD();
         static void LSD();
         static void filterAndTakeAverage(int start, int end, unsigned int windowSize, unsigned int threshold);
         static void fourPointTransform();
         static void inverse();
-        static void houghTransform();
-        static void takeAverage(int start, int end, unsigned int windowSize, unsigned int threshold);
-        static void checkForAllLanes();
+        static void initIPM();
     public:
         static void laneDetection();
         static bool setVideoSource(string source);
         static void videoIOStream();
-        static double getFrameCount();
 };
 
 #endif /* PROCESSING_HPP */
