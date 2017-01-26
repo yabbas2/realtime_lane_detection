270b503c27cfa78004f3d780048e58e6ccd1b9f8
3ebs <yousefahmedkamal94@gmail.com>
2017-01-26 15:59:41 +0200

    use CV_Assert on size of detectedLanes

--- src/IPM.cpp
+++ src/IPM.cpp
@@ -29,6 +29,6 @@ void Processing::inverse()
         py2 =  ((HomographyToOriginal.at<double>(1,0) * x[2] + HomographyToOriginal.at<double>(1,1) * x[3] + HomographyToOriginal.at<double>(1,2)) * Z);
 
         detectedLanes.push_back(Vec4d{px1, py1, px2, py2});
-        assert(detectedLanes.size() == size);
+        CV_Assert(detectedLanes.size() == size);
     }
 }
