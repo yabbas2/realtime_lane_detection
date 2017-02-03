3d525b5ccce8cc7cac483bb193c9caac6b9aed7e
3ebs <yousefahmedkamal94@gmail.com>
2017-02-03 16:13:11 +0200

    change color of text - add condition to show ipmFrame - change 'margin' to 'lineMargin'

--- src/Stream.cpp
+++ src/Stream.cpp
@@ -38,18 +38,18 @@ void Processing::videoIOStream()
             break;
         if(!detectedLanes.empty() && waitFlag)
         {
-            for (lane = detectedLanes.begin(); lane != detectedLanes.end() && margin > 0; lane++)
+            for (lane = detectedLanes.begin(); lane != detectedLanes.end() && lineMargin > 0; lane++)
             {
                 line(frameToShow, Point((int)(*lane)[0], (int)(*lane)[1]),
                                      Point((int)(*lane)[2], (int)(*lane)[3]),
                                              Scalar(255, 0, 0), 3, CV_AA);
             }
-            if(margin == 0)
-                putText(frameToShow, "Searching for lanes..", Point(640/3, 20), FONT_HERSHEY_SIMPLEX, 0.7,
-                        Scalar(0, 0, 255), 2, CV_AA);
-            if(margin > 0)
+            if(lineMargin == 0)
+                putText(frameToShow, "searching for lanes..", Point(640/3, 20), FONT_HERSHEY_SIMPLEX, 0.7,
+                        Scalar(255, 255, 0), 2, CV_AA);
+            if(lineMargin > 0)
                 putText(frameToShow, lanesNumber+"-lane road" , Point(640/3, 20), FONT_HERSHEY_SIMPLEX, 0.7,
-                        Scalar(0, 0, 255), 2, CV_AA);
+                        Scalar(255, 255, 0), 2, CV_AA);
             if(!arrow.empty())
                 arrowedLine(frameToShow, Point(arrow[0], arrow[1]),
                                             Point(arrow[2], arrow[3]), Scalar(0, 255, 0), 5, CV_AA, 0, 0.3);
@@ -59,7 +59,7 @@ void Processing::videoIOStream()
             imshow("Video", frameToShow);
         #endif
         #if show_ipm
-            imshow("IPM", ipmFrame);
+            if(!ipmFrame.empty()) imshow("IPM", ipmFrame);
         #endif
     }
     cout << "[INFO] End of streaming!" << endl;
