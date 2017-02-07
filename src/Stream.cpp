c32273f7b945f96cfdc6c031c2bfb50c2504c4c0
3ebs <yousefahmedkamal94@gmail.com>
2017-02-07 18:14:54 +0200

    implementation of draw method - new feedback technique

--- src/Stream.cpp
+++ src/Stream.cpp
@@ -2,6 +2,7 @@
 
 const char Processing::endStream = 'q';
 Mat Processing::normalFrame;
+Mat Processing::frameToShow;
 VideoCapture Processing::cap;
 double Processing::fps = 0;
 //double Processing::framesNumber = 0;
@@ -29,8 +30,6 @@ void Processing::videoIOStream()
 #if writeVideo
     VideoWriter video("out.avi",CV_FOURCC('M','J','P','G'),fps, Size(1280,360),true);
 #endif
-    list<Vec4f>::iterator lane;
-    Mat frameToShow;
     Mat finalFrame;
     while(true)
     {
@@ -40,25 +39,7 @@ void Processing::videoIOStream()
             break;
         if((waitKey((int)fps) & 0xFF) == (int)endStream)
             break;
-        if(!detectedLanes.empty() && waitFlag)
-        {
-            for (lane = detectedLanes.begin(); lane != detectedLanes.end() && lineMargin > 0; lane++)
-            {
-                line(frameToShow, Point((int)(*lane)[0], (int)(*lane)[1]),
-                                     Point((int)(*lane)[2], (int)(*lane)[3]),
-                                             Scalar(255, 0, 0), 3, CV_AA);
-            }
-            if(lineMargin == 0)
-                putText(frameToShow, "searching for lanes..", Point(640/3, 20), FONT_HERSHEY_SIMPLEX, 0.7,
-                        Scalar(255, 255, 0), 2, CV_AA);
-            if(lineMargin > 0)
-                putText(frameToShow, lanesNumber+"-lane road" , Point(640/3, 20), FONT_HERSHEY_SIMPLEX, 0.7,
-                        Scalar(255, 255, 0), 2, CV_AA);
-            if(!arrow.empty() && lineMargin > 0)
-                arrowedLine(frameToShow, Point(arrow[0], arrow[1]),
-                                            Point(arrow[2], arrow[3]), Scalar(0, 255, 0), 5, CV_AA, 0, 0.3);
-            waitFlag = false;
-        }
+        draw();
 #if show_final
         hconcat(normalFrame, frameToShow, finalFrame);
 #if writeVideo
@@ -74,4 +55,39 @@ void Processing::videoIOStream()
     cap.release();
     destroyWindow("Video");
     exit(0);
+}
+
+void Processing::draw()
+{
+    list<Vec4f>::iterator lane;
+    if(!detectedLanes.empty() && waitFlag)
+    {
+        for (lane = detectedLanes.begin(); lane != detectedLanes.end() && lineMargin > 0; lane++)
+        {
+            line(frameToShow, Point((int)(*lane)[0], (int)(*lane)[1]),
+                 Point((int)(*lane)[2], (int)(*lane)[3]),
+                 Scalar(255, 0, 0), 3, CV_AA);
+        }
+        rectangle(frameToShow, Point(0, 0), Point(200, 50), Scalar(0, 0, 0), 1, CV_AA);
+        putText(frameToShow, "status: ", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.4,
+                Scalar(255, 0, 0), 1, CV_AA);
+        putText(frameToShow, "AFPS: ", Point(10, 40), FONT_HERSHEY_SIMPLEX, 0.4,
+                Scalar(255, 0, 0), 1, CV_AA);
+        if(lineMargin == 0)
+            putText(frameToShow, "searching for lanes..", Point(60, 20), FONT_HERSHEY_SIMPLEX, 0.4,
+                    Scalar(0, 0, 255), 1, CV_AA);
+        if(lineMargin > 0)
+            putText(frameToShow, lanesNumber+"-lane road" , Point(60, 20), FONT_HERSHEY_SIMPLEX, 0.4,
+                    Scalar(0, 255, 0), 1, CV_AA);
+        if(afps >= fps)
+            putText(frameToShow, to_string(afps), Point(60, 40), FONT_HERSHEY_SIMPLEX, 0.4,
+                    Scalar(0, 255, 0), 1, CV_AA);
+        else
+            putText(frameToShow, to_string(afps), Point(60, 40), FONT_HERSHEY_SIMPLEX, 0.4,
+                    Scalar(0, 0, 255), 1, CV_AA);
+        if(!arrow.empty() && lineMargin > 0)
+            arrowedLine(frameToShow, Point(arrow[0], arrow[1]),
+                        Point(arrow[2], arrow[3]), Scalar(0, 255, 0), 5, CV_AA, 0, 0.3);
+        waitFlag = false;
+    }
 }
