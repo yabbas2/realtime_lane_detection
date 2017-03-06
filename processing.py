aa79b6859fdb0b5854473459b1e1252e906040b2
3ebs <yousefahmedkamal94@gmail.com>
2017-03-06 12:13:33 +0200

    fixed bugs - needs optimization

--- processing.py
+++ processing.py
@@ -6,18 +6,18 @@ import math
 def determinePtsAndDst(args, ext):
     [width, height] = [int(args['res'].split('x')[0]), int(args['res'].split('x')[1])]
     dst = np.array([[0, 0], [height, 0], [height, width], [0, width]], dtype="float32")
-    if args['video'].endswith("sample1"+ext):
+    if args['video'].endswith("sample1" + ext):
         pts = np.array([[253, 219], [352, 219], [444, 288], [60, 288]], dtype="float32")
-    elif args['video'].endswith("sample2"+ext):
+    elif args['video'].endswith("sample2" + ext):
         # pts = np.array([[197, 212], [326, 212], [348, 230], [106, 230]], dtype="float32")
         pts = np.array([[250, 222], [370, 222], [440, 290], [214, 290]], dtype="float32")
-    elif args['video'].endswith("sample3"+ext):
+    elif args['video'].endswith("sample3" + ext):
         # pts = np.array([[220, 200], [322, 200], [361, 230], [85, 230]], dtype="float32")
         pts = np.array([[250, 222], [370, 222], [440, 290], [214, 290]], dtype="float32")
-    elif args['video'].endswith("sample4"+ext):
+    elif args['video'].endswith("sample4" + ext):
         # pts = np.array([[230, 211], [342, 211], [373, 238], [160, 238]], dtype="float32")
         pts = np.array([[250, 222], [370, 222], [440, 290], [214, 290]], dtype="float32")
-    elif args['video'].endswith("sample5"+ext):
+    elif args['video'].endswith("sample5" + ext):
         # pts = np.array([[241, 208], [345, 208], [450, 297], [23, 297]], dtype="float32")
         pts = np.array([[260, 196], [354, 196], [442, 280], [204, 280]], dtype="float32")
 
@@ -66,25 +66,27 @@ def eliminateFalseDetection(lines):
     filtered_lines = []
     for line in lines:
         for x1, y1, x2, y2 in line:
-            theta = cv2.fastAtan2((y2-y1), (x2-x1))
+            theta = cv2.fastAtan2((y2 - y1), (x2 - x1))
             if theta > 180:
                 theta -= 180
-            length = math.sqrt(math.pow((x2-x1), 2) + math.pow((y2-y1), 2))
+            length = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
             if int(theta) in threshold_angle and length >= threshold_length:
-                filtered_lines.append([x1, y1, x2, y2])
+                if y1 > y2:
+                    filtered_lines.append([x1, y1, x2, y2])
+                else:
+                    filtered_lines.append([x2, y2, x1, y1])
     return filtered_lines
 
 
-def combineLineSegments(lines, threshold_position, image):
-    # lines = sorted(lines, key=lambda l: l[0], reverse=False)
+def combineLineSegments(lines, image):
     left_lines = []
     right_lines = []
-    threshold_angle = 1
+    threshold_position = int(image.shape[1] / 2)
+    threshold_angle = 2
     threshold_distance = 30
-    threshold_height = 5
     for lineI in lines:
         Ix1, Iy1, Ix2, Iy2 = lineI[0], lineI[1], lineI[2], lineI[3]
-        Itheta = int(cv2.fastAtan2((Iy2-Iy1), (Ix2-Ix1)))
+        Itheta = int(cv2.fastAtan2((Iy2 - Iy1), (Ix2 - Ix1)))
         if Itheta > 180:
             Itheta -= 180
         for lineJ in lines:
@@ -94,48 +96,58 @@ def combineLineSegments(lines, threshold_position, image):
             Jtheta = int(cv2.fastAtan2((Jy2 - Jy1), (Jx2 - Jx1)))
             if Jtheta > 180:
                 Jtheta -= 180
-            if abs(Jy1 - Iy1) <= threshold_height and abs(Jy2 - Iy2) <= threshold_height:
-                if abs(Jx1-Ix1) <= threshold_distance and abs(Jx2-Ix2) <= threshold_distance and \
-                        abs(Itheta-Jtheta) <= threshold_angle:
-                    avg_x1 = (Jx1+Ix1)/2
-                    avg_x2 = (Jx2+Ix2)/2
-                    avg_y1 = Jy1
-                    avg_y2 = Jy2
-                    # print(image[int(avg_y1)][int(avg_x1)])
-                    if image[int(avg_y1)][int(avg_x1)][0] in range(110, 255) and \
-                            image[int(avg_y1)][int(avg_x1)][1] in range(110, 255) and \
-                            image[int(avg_y1)][int(avg_x1)][2] in range(110, 255):
-                        if avg_x1 < threshold_position and avg_x2 < threshold_position:
-                            left_lines.append([avg_x1, avg_y1, avg_x2, avg_y2])
-                        else:
-                            right_lines.append([avg_x1, avg_y1, avg_x2, avg_y2])
-                        lines.remove(lineJ)
-            else:
-                if abs(Jx1 - Ix2) <= threshold_distance and abs(Jx2 - Ix1) <= threshold_distance and \
-                                abs(Itheta - Jtheta) <= threshold_angle:
-                    avg_x1 = (Jx1 + Ix2) / 2
-                    avg_x2 = (Jx2 + Ix1) / 2
-                    avg_y1 = Jy1
-                    avg_y2 = Jy2
-                    # print(image[int(avg_y1)][int(avg_x1)])
-                    if image[int(avg_y1)][int(avg_x1)][0] in range(110, 255) and \
-                            image[int(avg_y1)][int(avg_x1)][1] in range(110, 255) and \
-                            image[int(avg_y1)][int(avg_x1)][2] in range(110, 255):
-                        if avg_x1 < threshold_position and avg_x2 < threshold_position:
-                            left_lines.append([avg_x1, avg_y1, avg_x2, avg_y2])
-                        else:
-                            right_lines.append([avg_x1, avg_y1, avg_x2, avg_y2])
-                        lines.remove(lineJ)
+            if abs(Jx1 - Ix1) <= threshold_distance and abs(Jx2 - Ix2) <= threshold_distance and \
+                            abs(Itheta - Jtheta) <= threshold_angle:
+                avg_x1 = (Jx1 + Ix1) / 2
+                avg_x2 = (Jx2 + Ix2) / 2
+                avg_y1 = Jy1
+                avg_y2 = Jy2
+                avg_theta = (Jtheta + Itheta) / 2
+                # print(image[int(avg_y1)][int(avg_x1)])
+                if image[int((avg_y1 + avg_y2) / 2)][int((avg_x1 + avg_x2) / 2)][0] in range(110, 255) and \
+                        image[int((avg_y1 + avg_y2) / 2)][int((avg_x1 + avg_x2) / 2)][1] in range(110, 255) and \
+                        image[int((avg_y1 + avg_y2) / 2)][int((avg_x1 + avg_x2) / 2)][2] in range(110, 255):
+                    if avg_x1 < threshold_position and avg_x2 < threshold_position:
+                        left_lines.append([avg_x1, avg_y1, avg_x2, avg_y2, avg_theta])
+                    else:
+                        right_lines.append([avg_x1, avg_y1, avg_x2, avg_y2, avg_theta])
+                    # lines.remove(lineJ)
 
     left_points = []
     right_points = []
-    # left_lines = sorted(left_lines, key=lambda l: l[1], reverse=True)
-    for line in left_lines:
-        left_points.append([line[0], line[1]])
-        left_points.append([line[2], line[3]])
-    # right_lines = sorted(right_lines, key=lambda l: l[1], reverse=True)
-    for line in right_lines:
-        right_points.append([line[0], line[1]])
-        right_points.append([line[2], line[3]])
+    if len(left_lines) > 1:
+        sum_theta = 0
+        for line in left_lines:
+            sum_theta += ((line[3] - line[1]) / (line[2] - line[0]))
+        avg_theta = int(sum_theta / len(left_lines))
+        if abs(avg_theta - math.tan(left_lines[0][4])) <= math.tan(threshold_angle):
+            # slope = (left_lines[0][3]-left_lines[0][1]) / (left_lines[0][2]-left_lines[0][0])
+            new_x1 = (0 - left_lines[0][1]) / avg_theta + left_lines[0][0]
+            new_x2 = (image.shape[0] - left_lines[0][1]) / avg_theta + left_lines[0][0]
+            left_points = [[new_x1, 0], [new_x2, image.shape[0]]]
+        else:
+            for line in left_lines:
+                left_points.append([line[0], line[1]])
+                left_points.append([line[2], line[3]])
+    elif len(left_lines) == 1:
+        left_points.append([left_lines[0][0], left_lines[0][1]])
+        left_points.append([left_lines[0][2], left_lines[0][3]])
+
+    if len(right_lines) > 1:
+        sum_theta = 0
+        for line in right_lines:
+            sum_theta += ((line[3] - line[1]) / (line[2] - line[0]))
+        avg_theta = int(sum_theta / len(right_lines))
+        if abs(avg_theta - math.tan(right_lines[0][4])) <= math.tan(threshold_angle):
+            new_x1 = (0 - right_lines[0][1]) / avg_theta + right_lines[0][0]
+            new_x2 = (image.shape[0] - right_lines[0][1]) / avg_theta + right_lines[0][0]
+            right_points = [[new_x1, 0], [new_x2, image.shape[0]]]
+        else:
+            for line in right_lines:
+                right_points.append([line[0], line[1]])
+                right_points.append([line[2], line[3]])
+    elif len(right_lines) == 1:
+        right_points.append([right_lines[0][0], right_lines[0][1]])
+        right_points.append([right_lines[0][2], right_lines[0][3]])
 
     return left_points, right_points
