b47b53e71c918db5c0c25dbb81e670016af761b5
3ebs <yousefahmedkamal94@gmail.com>
2017-03-07 19:32:19 +0200

    added regular expression for automation

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import argparse
 from stream import *
 from processing import *
+import re
 import time
 
 
 arg = argparse.ArgumentParser()
 arg.add_argument('-v', '--video', type=str, help="Video source")
-arg.add_argument('-r', '--res', type=str, help='video resolution')
 args = vars(arg.parse_args())
 
 
-print("[INFO]   application starts")
+print("[INFO] application starts")
 
 if args['video'] is not None:
     video = stream(src=args['video'])
 else:
     video = stream(src=0)
 video.start()
-fps = int(video.getInfo())
-pts, dst = determinePtsAndDst(args, ".mp4")
+fps, width, height, frameCount = video.getInfo()
+sample = re.findall("[a-z A-Z0-9\\\-_?&()#@/]+(sample[0-9]+).[a-z0-9]+", args['video'])
+pts, dst = determinePtsAndDst(width, height, ''.join(sample))
 afps = 0
 while True:
     start = time.time()
     if not video.getStreamStatus():
-        print("[INFO]   application ends")
+        print("[INFO] application ends")
         exit(0)
     frame = video.readFrame()
     if frame is None:
@@ -38,4 +39,4 @@ while True:
     end = time.time()
     afps = int(1 / (end - start))
     video.setInfo(left_points, right_points, afps)
-    print('[INFO]   Time is: %s' % (end-start))
+    # print('[INFO]   Time is: %s' % (end-start))
