ea3cea362a580564db8e9592d9d108e49d719d4a
meegz <mohamed_magdy1696@hotmail.com>
2017-04-30 17:55:05 +0200

    .h and declared in .cpp

--- back-end/region_growing.cpp
+++ back-end/region_growing.cpp
@@ -3,11 +3,55 @@
 
 RegionGrowing::RegionGrowing(){}
 
+void RegionGrowing::regionGrowing(vector<Vec4i> &l)
+{
 
+}
 
+Vec4i RegionGrowing::findLeftSeedLines()
+{
 
+}
 
+Vec4i RegionGrowing::findRightSeedLines()
+{
 
+}
+
+void RegionGrowing::leftRegionGrowing()
+{
+
+}
+
+void RegionGrowing::rightRegionGrowing()
+{
+
+}
+
+vector<Vec4i> RegionGrowing::getLines()
+{
+
+}
+
+Vec4i RegionGrowing::getLeftSeedLines()
+{
+
+}
+
+Vec4i RegionGrowing::getRightSeedLines()
+{
+
+}
+
+vector<Vec4i> RegionGrowing::getLeftRegion()
+{
+
+}
+
+vector<Vec4i> RegionGrowing::getRightRegion()
+{
+
+}
 
 //sort(lines.begin(), lines.end(),
 //              [](const std::vector<float>& a, const std::vector<float>& b) {
