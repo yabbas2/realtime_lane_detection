7dbbd0662183f1cd2e2bf5abc4598539dae3a8ac
3ebs <yousefahmedkamal94@gmail.com>
2017-06-28 17:58:40 +0200

    moved filtration to reg process - added ipm frame to SM with reg process

--- detection.cpp
+++ detection.cpp
@@ -7,8 +7,6 @@ Detection::Detection(int &argc, char **argv) :
     log.write("----------------------------------------------------------");
     log.write("------------------------NEW RUN---------------------------");
     log.write("----------------------------------------------------------");
-    boundary_min = {75, 100, 0};
-    boundary_max = {112, 225, 154};
     ifMaster = new QDBusInterface("com.stage.master", "/", "com.stage.master", bus, this);
     ifReg = new QDBusInterface("com.stage.reg", "/", "com.stage.reg", bus2, this);
     QDBusReply<QString> key = ifMaster->call("getDETECTIONREGKEY");
@@ -34,75 +32,24 @@ void Detection::lineSegmentDetector()
     cvtColor(inputFrame, processedFrame, COLOR_BGR2GRAY);
     GaussianBlur(processedFrame, processedFrame, Size(9, 9), 1, 2);
     lsd->detect(processedFrame, lines);
-    filterPhaseOne();
-    filterPhaseTwo();
+    uchar *ipmData = inputFrame.data;
     while (!sm.lock());
     sharedData *sData = (sharedData*) sm.data();
-    sData->actualSize = (phaseTwoFiltered.size() >= LINES_MAX_SIZE) ? LINES_MAX_SIZE : phaseTwoFiltered.size();
+    sData->actualSize = (lines.size() >= LINES_MAX_SIZE) ? LINES_MAX_SIZE : lines.size();
     for (int i = 0; i < sData->actualSize; ++i)
-        for (int j = 0; j < 6; ++j)
-            sData->lineSegments[i][j] = phaseTwoFiltered[i][j];
+    {
+        sData->lineSegments[i][0] = (int) lines[i][0];
+        sData->lineSegments[i][1] = (int) lines[i][1];
+        sData->lineSegments[i][2] = (int) lines[i][2];
+        sData->lineSegments[i][3] = (int) lines[i][3];
+    }
+    memcpy(sData->ipmData, ipmData, IPM_FRAME_SIZE);
     sm.unlock();
-    ifReg->call("RGCFV");
+    ifReg->call("FRGCFV");
     t2 = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(t2 - t1).count();
     log.write("[DETECTION] frame no. " + QString::number(frameCount) + ", exec time: " + QString::number(duration));
     busy = false;
-}
-
-void Detection::filterPhaseOne()
-{
-    phaseOneFiltered.clear();
-    for(unsigned int it = 0; it < lines.size(); ++it)
-    {
-        float x1 = lines[it][0], y1 = lines[it][1], x2 = lines[it][2], y2 = lines[it][3];
-        if(y1 < y2)
-        {
-            swap(x1, x2);
-            swap(y1, y2);
-        }
-        float theta = fastAtan2((y2 - y1), (x2 - x1));
-        if (theta > 180)
-            theta -= 180;
-        float length = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-        if ((threshold_angle_min < theta) && (theta < threshold_angle_max) && (length >= threshold_length))
-            phaseOneFiltered.push_back(Vec6i{(int)x1, (int)y1, (int)x2, (int)y2, (int)theta, (int)length});
-    }
-}
-void Detection::filterPhaseTwo()
-{
-    phaseTwoFiltered.clear();
-    vector<vector<Point>> contours;
-    cvtColor(inputFrame, processedFrame, COLOR_BGR2HSV);
-    inRange(processedFrame, boundary_min, boundary_max, processedFrame);
-    findContours(processedFrame, contours, 1, 2);
-    if(contours.empty())
-    {
-        phaseTwoFiltered = phaseOneFiltered;
-        return;
-    }
-    for(unsigned int it_lines = 0; it_lines < phaseOneFiltered.size(); ++it_lines)
-    {
-        bool flag = false;
-        for(unsigned int it_contours = 0; it_contours < contours.size(); ++it_contours)
-        {
-            if (contourArea(contours[it_contours]) > 10000 || contourArea(contours[it_contours]) < 1000)
-                continue;
-            Point2f a((float)phaseOneFiltered[it_lines][0], (float)phaseOneFiltered[it_lines][1]);
-            Point2f b((float)phaseOneFiltered[it_lines][2], (float)phaseOneFiltered[it_lines][3]);
-            double dist1 = pointPolygonTest(contours[it_contours], a, true);
-            double dist2 = pointPolygonTest(contours[it_contours], b, true);
-            if(dist1 >= -17 || dist2 >= -17)
-            {
-                flag = true;
-                break;
-            }
-        }
-        if(!flag)
-            phaseTwoFiltered.push_back(Vec6i{phaseOneFiltered[it_lines][0], phaseOneFiltered[it_lines][1],
-                                             phaseOneFiltered[it_lines][2], phaseOneFiltered[it_lines][3],
-                                             phaseOneFiltered[it_lines][4], phaseOneFiltered[it_lines][5]});
-    }
 }
 
 void Detection::cannyHough(int side)
--- detection.h
+++ detection.h
@@ -9,10 +9,7 @@
 #include <chrono>
 #include "../logger/logger.h"
 
-#define threshold_angle_min     70
-#define threshold_angle_max     110
-#define threshold_length        50
-#define LINES_MAX_SIZE          50
+#define LINES_MAX_SIZE          100
 #define IPM_FRAME_SIZE          1152000
 #define frameHeight             480
 #define frameWidth              800
@@ -26,26 +23,19 @@ namespace detectionSide {
     enum {left, right, both};
 }
 
-typedef Vec<int, 6> Vec6i;
-
 class Detection : public QApplication
 {
 public:
     Detection(int &argc, char **argv);
     void lineSegmentDetector();
     void cannyHough(int side);
-    void wa2();
     bool busy;
 
 private:
     Mat inputFrame;
     Mat processedFrame;
-    vector<int> boundary_max;
-    vector<int> boundary_min;
     Ptr<LineSegmentDetector> lsd;
     vector<Vec4f> lines;
-    vector<Vec6i> phaseOneFiltered;
-    vector<Vec6i> phaseTwoFiltered;
     vector<Vec4f> tmpLeftLines;
     vector<Vec4f> tmpRightLines;
     QDBusConnection bus = QDBusConnection::sessionBus();
@@ -53,7 +43,8 @@ private:
     QSharedMemory sm;
     struct sharedData {
         int actualSize;
-        int lineSegments[LINES_MAX_SIZE][6];
+        int lineSegments[LINES_MAX_SIZE][4];
+        uchar ipmData[IPM_FRAME_SIZE];
     };
     QDBusConnection bus2 = QDBusConnection::sessionBus();
     QDBusInterface *ifReg;
@@ -65,9 +56,6 @@ private:
     high_resolution_clock::time_point t1;
     high_resolution_clock::time_point t2;
     int frameCount;
-
-    void filterPhaseOne();
-    void filterPhaseTwo();
 };
 
 #endif // LINEDETECTION_H
